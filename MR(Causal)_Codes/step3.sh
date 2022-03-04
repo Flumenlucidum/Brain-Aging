cf3dc8aa730d97815af0f06d8bb404e0588943cb
Flumenlucidum <49855759+Flumenlucidum@users.noreply.github.com>
2022-03-04 14:02:32 +0900

    Delete MR(Causal)_Codes directory

--- MR(Causal)_Codes/step3.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for ((i=1;i<=22;i++));
-do
-	nohup /media/leelabsg-storage0/jangho/plink2 --bfile MR_not_pruned_chr"$i"  --indep-pairwise 50 5 0.01    --out MR_list_pruned_chr"$i" > log_chr"$i" &
-done
--- MR(Causal)_Codes/step4.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for ((i=1;i<=22;i++));
-do
-	nohup /media/leelabsg-storage0/jangho/plink2 --bfile MR_not_pruned_chr"$i"  --extract MR_list_pruned_chr"$i".prune.in  --make-bed  --out MR_pruned_chr"$i" > log_chr"$i" &
-done
